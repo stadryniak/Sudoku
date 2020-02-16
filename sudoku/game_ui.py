d8107229fe544d1ec700840cf0b7015b90992c6d
Michał Stadryniak <stadryniak.michal@gmail.com>
2020-02-16 16:47:15 +0100

    start values print

--- sudoku/game_ui.py
+++ sudoku/game_ui.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from pygame.locals import *
+from sudoku import sudoku_game
 
 # Sets background color
 BACKGROUND = (255, 255, 255)
@@ -29,7 +30,7 @@ def draw_grid():
     # Draw minor lines
     for x in range(0, WINDOWWIDTH, CELLSIZE):  # draw vertical lines
         pygame.draw.line(DISPLAYSURF, GRAY, (x, 0), (x, WINDOWHEIGHT))
-        if x % SQUARESIZE == 0:
+        if x % SQUARESIZE == 0:  # draw major lines
             pygame.draw.line(DISPLAYSURF, BLACK, (x, 0), (x, WINDOWHEIGHT))
     for y in range(0, WINDOWHEIGHT, CELLSIZE):
         pygame.draw.line(DISPLAYSURF, GRAY, (0, y), (WINDOWWIDTH, y))
@@ -38,9 +39,41 @@ def draw_grid():
     return None
 
 
-def main_ui():
+def populate_cells(middles, data):
+    i = 0
+    j = 0
+    for row in middles:
+        for mid in row:
+            if data[i][j]:
+                font = pygame.font.Font('freesansbold.ttf', 30)
+                text = font.render(str(data[i][j]), True, BLACK, WHITE)
+                text_rect = text.get_rect()
+                text_rect.center = mid
+                DISPLAYSURF.blit(text, text_rect)
+            j += 1
+        i += 1
+        j = 0
+
+
+def cells_middle():
+    middle = [CELLSIZE // 2, CELLSIZE // 2]
+    res = []
+    for i in range(9):
+        res.append([])
+        for j in range(9):
+            res[i].append((middle[0], middle[1]))
+            middle[0] += CELLSIZE
+        middle[1] += CELLSIZE
+        middle[0] = CELLSIZE // 2
+    return res
+
+
+def main_ui(game):
     global FPSCLOCK, DISPLAYSURF
+
     draw_grid()
+    cells_mid = cells_middle()
+    populate_cells(cells_mid, game.board)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
--- sudoku/main.py
+++ sudoku/main.py
@@ -2,4 +2,16 @@ from sudoku import sudoku_game
 from sudoku import game_ui
 
 if __name__ == "__main__":
-    game_ui.main_ui()
+    game = sudoku_game.SudokuBoard()
+    game.board = [
+        [9, 0, 0, 0, 8, 0, 3, 0, 0],
+        [0, 0, 0, 2, 5, 0, 7, 0, 0],
+        [0, 2, 0, 3, 0, 0, 0, 0, 4],
+        [0, 9, 4, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 7, 3, 0, 5, 6, 0],
+        [7, 0, 5, 0, 6, 0, 4, 0, 0],
+        [0, 0, 7, 8, 0, 3, 9, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+    game_ui.main_ui(game)
